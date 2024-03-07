9efce10d8a6cf58b6042cf3ae937b5792fa3fd70
Nameer2 <162543220+Nameer2@users.noreply.github.com>
2024-03-07 16:45:37 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144535
+let accountEmail = "nameershahid15@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2//notallowed
+ console.log(accountId);
+accountEmail="hchc@gmail.com"
+accountPassword="34"
+accountCity="lahore"
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)//we can,t use it as we are using nodejs not browser
+
+let name = "nameer"
+let age = 19
+let isloggedin =true
+
+// number => 2 t0 power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stand alone value
+//undefined =>
+//symbol => unique
+
+//object 
+
+console.log(typeof "nameer")//string
+console.log(typeof undefined);//undefines
+console.log(typeof null);//object
