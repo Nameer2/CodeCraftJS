0ca9810478fa524a79d5755a96b48eced81ac5ba
Nameer2 <162543220+Nameer2@users.noreply.github.com>
2024-03-08 12:34:06 +0000

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,19 @@
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3); for power
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " nameer"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
