d9b621d91761e614d7551c7f50f459bcde93789c
Nameer2 <162543220+Nameer2@users.noreply.github.com>
2024-03-08 07:56:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+/*let score=true
+console.log(typeof score)
+console.log(score)
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)*/
+
+
+//"33"easily convert
+//"abc"=> nan
+// true => 1; false =>0
+// null => 0
+
+/* let isloggedin = "nameer"
+
+let booleanIsloggedin = Boolean(isloggedin)
+console.log(booleanIsloggedin)*/
+
+//1 => true ; o=> false
+//"" =>false
+// "nameer" => true
+
+
+let num = 33
+
+let stringnum = String(num)
+
+console.log(typeof stringnum);
