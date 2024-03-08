69068e0731e3f899a0c3a2226072f13f04405ce6
Nameer2 <162543220+Nameer2@users.noreply.github.com>
2024-03-08 13:31:21 +0000

    comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,13 @@
+console.log("2"> 1); //true , "2" automatically converted to 2
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); //true
+console.log(null <= 0); //true
+
+// if we place undefined instead of null it always give false
+
+// === strict check
+
+console.log("2" === 2); //false if we put two equal then ans is true
+console.log("2" == 2);
