2a02a2ad46542b450b5bf7bac0fff3f56a296d8c
Nameer2 <162543220+Nameer2@users.noreply.github.com>
2024-03-08 13:47:38 +0000

    summary

---
+++ 01_basics/06_summary.js
@@ -0,0 +1,3 @@
+// there are two types of data type , 1 primitive , 2 non-primitive/refrence type
+// primitive 7 types : String , Number , Boolean , null , undefined , Symbol , Bigint(for largest value)
+// refrence type : array , objects , functions 
