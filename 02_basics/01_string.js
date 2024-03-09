d92dc1f1b6c8814391f8b25324d99cc9b7af5c9f
Nameer2 <162543220+Nameer2@users.noreply.github.com>
2024-03-09 16:35:46 +0000

    string

---
+++ 02_basics/01_string.js
@@ -0,0 +1,23 @@
+const name ="Nameer"
+const repoCount =  50
+console.log(name + repoCount + "18"); // not required
+console.log(`hello my name is ${name} and my repecount is ${repoCount}`);
+const gameName = new String('nameer-hc') // another way of string declaration
+console.log(gameName [0])
+
+console.log(gameName.length);//to check lenghnth
+console.log(gameName.toUpperCase()); //to convert in upercase
+
+console.log(gameName.charAt(3));//to  check what is in index
+console.log(gameName.indexOf('m'));//to check index of value
+
+const newString = gameName.substring(0,4)//it print only 0 1 2 3 index of name
+console.log(newString);
+
+const newstringone = "     nameer     "
+console.log(newstringone);
+console.log(newstringone.trim());//remove spaces in start or end
+
+const rollno=new String("293598 23006481 23006465")
+console.log(rollno.split())
+
